87521c5ec720ae9214b955de896bab920924eed7
Jyrki Kajala <jyrki.kajala@gmail.com>
2013-11-18 22:12:06 +0200

    s97 Refactor using URL includes
    	new file:   lists/urls.py
    	modified:   superlists/urls.py

---
+++ lists/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^(.+)/$',         'lists.views.view_list', name='view_list'),
+    url(r'^(.+)/new_item$', 'lists.views.add_item',  name='add_item'),
+    url(r'^new$',           'lists.views.new_list',  name='new_list'),
+)
