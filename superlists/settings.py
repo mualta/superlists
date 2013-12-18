b9fe39968adbe653f837c0d63419a4bfd9b51ef6
Jyrki Kajala <jyrki.kajala@gmail.com>
2013-12-18 09:57:23 +0200

    settings.py: DEBUG=false ALLOWED_HOSTS=['localhost']

--- superlists/settings.py
+++ superlists/settings.py
@@ -3,7 +3,10 @@
 from os import path
 
 PROJECT_ROOT = path.abspath(path.join(path.dirname(__file__), '..'))
-DEBUG = True
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -26,12 +29,13 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
+
 TIME_ZONE = 'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
