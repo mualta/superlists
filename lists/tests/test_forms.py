162583995d5dc30c0b12019d52146a26884ab342
Jyrki Kajala <jyrki.kajala@gmail.com>
2013-12-23 22:25:47 +0200

    new form for list items, page 172

---
+++ lists/tests/test_forms.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from lists.forms import EMPTY_LIST_ERROR, ItemForm
+
+
+
+
+class ItemFormTest(TestCase):
+
+	def test_form_validation_for_blank_items(self):
+		form = ItemForm(data={'text': ''})
+		self.assertFalse(form.is_valid())
+		self.assertEqual(form.errors['text'], [EMPTY_LIST_ERROR])
