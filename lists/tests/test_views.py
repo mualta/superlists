1f4716d25857d83ed76010cb4be088e0e875ad11
Jyrki Kajala <jyrki.kajala@gmail.com>
2013-12-30 12:49:57 +0200

    sivu 180: Extra test for not saving List on invalid input

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -156,13 +156,11 @@ class NewListTest(TestCase):
 
 	def test_validation_errors_sent_back_to_home_page_template(self):
 		response = self.client.post('/lists/new', data={'text': ''})
+		self.assertEqual(List.objects.all().count(), 0)
 		self.assertEqual(Item.objects.all().count(), 0)
 		self.assertTemplateUsed(response, 'home.html')
-		error = escape(EMPTY_LIST_ERROR)
-		self.assertContains(response, error)
-		self.assertIsInstance(response.context['form'], ItemForm)
+		self.assertContains(response, escape(EMPTY_LIST_ERROR))
 
-		
 
 
 
@@ -171,12 +169,10 @@ class NewItemTest(TestCase):
 
 	def test_can_save_a_POST_request_to_an_existing_list(self):
 		correct_list = List.objects.create()
-
 		self.client.post(
 			'/lists/%d/new_item' % (correct_list.id,),
 			data={'text': 'A new item for an existing list'}
 		)
-
 		self.assertEqual(Item.objects.all().count(), 1)
 		new_item = Item.objects.all()[0]
 		self.assertEqual(new_item.text, 'A new item for an existing list')
